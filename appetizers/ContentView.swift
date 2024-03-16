290a84402160bf4b0793fbc87c170e8b7a642c0c
Vishnu Priyan S S <vishnu32510@gmail.com>
2024-03-15 20:28:50 -0500

    Initial Commit

---
+++ appetizers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  appetizers
+//
+//  Created by Vishnu Priyan Sellam Shanmugavel on 3/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ appetizers/appetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  appetizersApp.swift
+//  appetizers
+//
+//  Created by Vishnu Priyan Sellam Shanmugavel on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct appetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
